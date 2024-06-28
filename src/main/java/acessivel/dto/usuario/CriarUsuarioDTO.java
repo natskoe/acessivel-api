ebec1464301db230836044f88a0c67688ae39999
natskoe <koehlernathy@gmail.com>
2024-06-28 01:24:24 -0300

    Anotação para validar entradas que não podem ser nulas.

--- src/main/java/acessivel/dto/usuario/CriarUsuarioDTO.java
+++ src/main/java/acessivel/dto/usuario/CriarUsuarioDTO.java
@@ -3,6 +3,9 @@ package acessivel.dto.usuario;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Date;
@@ -12,11 +15,23 @@ import java.util.Date;
 @AllArgsConstructor
 public class CriarUsuarioDTO {
 
+    @NotBlank(message = "Nome não pode estar vazio.")
     private String nome;
+
+    @NotBlank(message = "Sobrenome não pode estar vazio.")
     private String sobrenome;
+
+    @NotBlank(message = "CPF não pode estar vazio.")
+    @Size(min = 11, max = 11)
     private String cpf;
+
+    @NotBlank(message = "Email não pode estar vazio.")
+    @Email
     private String email;
+
+    @NotBlank(message = "Senha não pode estar vazio.")
     private String senha;
+
     @Basic
     @Temporal(TemporalType.DATE)
     private Date dataNascimento;
